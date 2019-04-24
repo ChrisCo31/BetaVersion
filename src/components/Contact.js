349e9d4d5c650984d2cfff9a3dbf9f0633485721
ChrisCo31 <christel.coeur@free.fr>
2019-04-24 13:35:28 +0200

    sup anti spam
    data-netlify-honeypot="bot-field"
    <input type="hidden" name="bot-field" />

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,8 +4,8 @@ const Contact = (props) => (
     <section id="contact">
         <div className="inner">
             <section>
-            <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
-                <input type="hidden" name="bot-field" />
+            <form name="contact" method="post" data-netlify="true" >
+                
                 <input type="hidden" name="form-name" value="contact" />
                     <div className="field half first">
                         <label htmlFor="name">Nom</label>
